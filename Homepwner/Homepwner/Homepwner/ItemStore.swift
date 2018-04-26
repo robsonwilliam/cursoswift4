daae91cb9da2bec225cea085bbf0fc983d530ef8
Robson William <robson.dba@gmail.com>
2018-04-26 14:56:28 -0300

    Capítulo 11 sem desafios

--- Homepwner/Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/Homepwner/ItemStore.swift
@@ -19,9 +19,28 @@ class ItemStore {
     }
     
     init() {
+        /*
         for _ in 0..<5 {
             createItem()
         }
+        //após adicionar o botao de incluir, não há mais necessidade de inicializar a lista
+        */
+    }
+ 
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
     }
     
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
 }
