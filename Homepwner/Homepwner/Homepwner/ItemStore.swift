0044061982245d09bd6851f850f5ee8e75f76353
Robson William <robson.dba@gmail.com>
2018-04-26 11:21:27 -0300

    Capitulo 10 sem desafios

---
+++ Homepwner/Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by T1aluno10 on 26/04/18.
+//  Copyright © 2018 T1aluno10. All rights reserved.
+//
+
+import UIKit
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+}
---
+++ Homepwner/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by T1aluno10 on 26/04/18.
+//  Copyright © 2018 T1aluno10. All rights reserved.
+//
+
+import UIKit
+class ItemsViewController: UITableViewController {
+    var itemStore: ItemStore!
+        
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell, with default appearance
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+                                                 for: indexPath)
+        
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$ \(item.valueInDollars)"
+        return cell }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+}
