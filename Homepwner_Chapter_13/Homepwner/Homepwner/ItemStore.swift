3b3370442b4f484bd8da8c35d74a91117053d107
Robson William <robson.dba@gmail.com>
2018-04-27 14:30:14 -0300

    Capitulo 13 mais notas

---
+++ Homepwner_Chapter_13/Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,46 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by T1aluno10 on 26/04/18.
+//  Copyright © 2018 T1aluno10. All rights reserved.
+//
+
+import UIKit
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        /*
+        for _ in 0..<5 {
+            createItem()
+        }
+        //após adicionar o botao de incluir, não há mais necessidade de inicializar a lista
+        */
+    }
+ 
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+}
---
+++ Homepwner_Chapter_13/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,174 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by T1aluno10 on 26/04/18.
+//  Copyright © 2018 T1aluno10. All rights reserved.
+//
+
+import UIKit
+class ItemsViewController: UITableViewController /*, UITableViewDelegate*/ {
+    var itemStore: ItemStore!
+    
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        /*
+        // Make a new index path for the 0th section, last row
+        let lastRow = tableView.numberOfRows(inSection: 0)
+        let indexPath = IndexPath(row: lastRow, section: 0)
+        // Insert this new row into the table
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        */
+        
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell, with default appearance
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        
+        // Get a new or recycled cell
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        
+        let item = itemStore.allItems[indexPath.row]
+        
+        
+        
+        let colorGreen = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+        // This is a translucent black.
+        let colorRed = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+        
+        
+        
+        //cell.textLabel?.text = item.name
+        //cell.detailTextLabel?.text = "$ \(item.valueInDollars)"
+        
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        
+        
+        cell.valueLabel.text = "$ \(item.valueInDollars)"
+        
+        if item.valueInDollars < 50 {
+            cell.valueLabel.textColor = colorGreen
+        } else {
+            cell.valueLabel.textColor = colorRed
+        }
+        
+        
+        return cell }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        //var tableFooterView:UITableView
+        
+        //self.tableView.tableFooterView = tableFooter
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
+        
+        //tableView.rowHeight = 65
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
+        
+    }
+    
+    //func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+     //
+    //}
+    
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title,
+                                       message: message,
+                                       preferredStyle: .actionSheet /*.alert*/)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: NSLocalizedString("Remove", comment: "Removing Item"), style: .destructive,
+                                             handler: { (action) -> Void in
+                                                // Remove the item from the store
+                                                self.itemStore.removeItem(item)
+                                                // Also remove that row from the table view with an animation
+                                                self.tableView.deleteRows(at: [indexPath], with: .automatic) })
+            ac.addAction(deleteAction)
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+            
+            // Remove the item from the store
+            //itemStore.removeItem(item)
+            // Also remove that row from the table view with an animation
+            //tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            // Update the model
+        to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If the triggered segue is the "showItem" segue
+        switch segue.identifier {
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController
+                    = segue.destination as! DetailViewController
+                detailViewController.item = item
+            } default:
+                preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+    
+}
